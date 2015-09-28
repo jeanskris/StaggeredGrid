48d83f1d1d3ef4c8b2bf5807a0ada52705169ecf
Jeankris <zfzwpl5860053>
2015-09-28 10:29:22 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication2/app/SampleData.java
@@ -0,0 +1,19 @@
+package com.example.myapplication2.app;
+
+import java.util.ArrayList;
+
+public class SampleData {
+
+    public static final int SAMPLE_DATA_ITEM_COUNT = 30;
+
+    public static ArrayList<String> generateSampleData() {
+        final ArrayList<String> data = new ArrayList<String>(SAMPLE_DATA_ITEM_COUNT);
+
+        for (int i = 0; i < SAMPLE_DATA_ITEM_COUNT; i++) {
+            data.add("SAMPLE #");
+        }
+
+        return data;
+    }
+
+}
